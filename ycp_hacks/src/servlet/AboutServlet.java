be3254a75e556852142421dda66e11bfffc70a82
Walrussuit101 <tjefferson@ycp.edu>
2019-03-04 23:47:31 -0500

    add about page is '/about'

---
+++ ycp_hacks/src/servlet/AboutServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AboutServlet extends HttpServlet{
+private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("About Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/about.jsp").forward(req, resp);
+	}
+}
