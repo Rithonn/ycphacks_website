472b12dff67777af9dbfdb465a4343c0d467a689
Walrussuit101 <tjefferson@ycp.edu>
2019-03-05 00:09:44 -0500

    added registration page is '/registration'

---
+++ ycp_hacks/src/servlet/RegistrationServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegistrationServlet extends HttpServlet{
+private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Registration Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/registration.jsp").forward(req, resp);
+	}
+}
---
+++ ycp_hacks/war/_view/registration.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+
+<html>
+	<head>
+		<title>Registration</title>
+	</head>
+	
+	<body>
+		placeholder
+	</body>
+</html>
