2e489fb8eaf7f5dff904657f5f6fbdc207e3bdaf
Walrussuit101 <tjefferson@ycp.edu>
2019-03-22 11:38:54 -0400

    started populating user class

--- ycp_hacks/src/model/User.java
+++ ycp_hacks/src/model/User.java
@@ -1,5 +1,20 @@
 package model;
 
 public class User {
-
+	private String email;
+	private String password;
+	private int age;
+	private String university;
+	private boolean isRegStatus;
+	//profType?
+	
+	
+	
+	public User() {
+	}
+	
+	
+	
+	
+	
 }
