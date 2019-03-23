62aae740cc49695402f71fffbc8fda26b7a07909
Walrussuit101 <tjefferson@ycp.edu>
2019-03-23 14:01:41 -0400

    got sessions working, makes a currentUser model for every jsp

--- ycp_hacks/src/servlet/IndexServlet.java
+++ ycp_hacks/src/servlet/IndexServlet.java
@@ -6,16 +6,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
+	HttpSession session = null;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
 		System.out.println("Index Servlet: doGet");
-		
+		session = req.getSession();
 		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
 	}
 }
