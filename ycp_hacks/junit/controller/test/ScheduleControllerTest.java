a9f54b0281394deeff71a15f57c978d856e4b55a
cbrandt6 <cbrandt6@ycp.edu>
2019-03-24 20:32:35 -0400

    Created models and controllers as well as test files for each for schedule and event

---
+++ ycp_hacks/junit/controller/test/ScheduleControllerTest.java
@@ -0,0 +1,14 @@
+package controller.test;
+
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import model.Schedule;
+public class ScheduleControllerTest{
+	
+	@Before
+	public void setup() {
+		
+	}
+}
