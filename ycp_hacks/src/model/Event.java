64a3558dc26b10477513e4b0c356c49715bc084e
cbrandt6 <cbrandt6@ycp.edu>
2019-04-02 21:08:37 -0400

    Created the event class and added getters and setters

--- ycp_hacks/src/model/Event.java
+++ ycp_hacks/src/model/Event.java
@@ -1,5 +1,50 @@
 package model;
 
 public class Event{
+	//Event fields
+	private String location;
+	private String time;
+	private String name;
+	private String description;
+	private Boolean passedTime;
+	private Boolean upComing;
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Boolean isPassedTime() {
+		return passedTime;
+	}
+	public void setPassedTime(Boolean passedTime) {
+		this.passedTime = passedTime;
+	}
+	public Boolean isUpComing() {
+		return upComing;
+	}
+	public void setUpComing(Boolean upComing) {
+		this.upComing = upComing;
+	}
+	
 	
 }
