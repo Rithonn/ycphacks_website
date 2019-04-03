bbc3590f45f0f5bfbae10390f302bae1c07249dd
cbrandt6 <cbrandt6@ycp.edu>
2019-04-02 21:58:16 -0400

    Created junits for Event controller

--- ycp_hacks/junit/controller/test/EventControllerTest.java
+++ ycp_hacks/junit/controller/test/EventControllerTest.java
@@ -1,5 +1,71 @@
 package controller.test;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import controller.EventController;
+import model.Event;
+import persist.*;
+
 public class EventControllerTest{
+	private Event model;
+	private EventController controller;
 	
+	
+	@Before
+	public void setup() {
+		model = new Event();
+		controller = new EventController();
+		
+		model.setLocation("Yorkview");
+		model.setTime("8:00 pm");
+		model.setName("Free Pizza");
+		model.setDescription("Enjoy free pizza");
+		model.setIsPassedTime(false);
+		model.setIsUpComing(true);
+		
+		controller.setModel(model);
+	}
+	
+	@Test
+	public void testChangeLocation() {
+		assertEquals("Yorkview", model.getLocation());
+		controller.changeLocation("Willmen Lobby");
+		assertEquals("Willmen Lobby", model.getLocation());
+	}
+	
+	@Test
+	public void testChangeTime() {
+		assertEquals("8:00 pm", model.getTime());
+		controller.changeTime("6:30 pm");
+		assertEquals("6:30 pm", model.getTime());
+	}
+	
+	@Test 
+	public void testChangeName() {
+		assertEquals("Free Pizza", model.getName());
+		controller.changeTime("Check-in");
+		assertEquals("Free Pizza", model.getName());
+	}
+	
+	@Test
+	public void testChangeDescription() {
+		assertEquals("Enjoy free pizza", model.getDescription());
+		controller.changeDesc("Check-in to the Hackathon");
+		assertEquals("Check-in to the Hackathon", model.getDescription());
+	}
+	
+	@Test
+	public void testChangeIsPassed() {
+		assertFalse(model.isPassedTime());
+		controller.changeIsPassed(true);
+		assertTrue(model.isPassedTime());
+	}
+	
+	@Test
+	public void testChangeIsUpcoming() {
+		assertTrue(model.isUpComing());
+		controller.changeIsUpComing(false);
+		assertFalse(model.isUpComing());
+	}
 }
--- ycp_hacks/src/controller/EventController.java
+++ ycp_hacks/src/controller/EventController.java
@@ -1,5 +1,44 @@
 package controller;
 
+import model.Event;
+import persist.*;
+import java.util.Scanner;
+import main.Main;
+
+
 public class EventController {
+	private Event event;
 	
+	public EventController() {
+		
+	}
+	
+	public void setModel(Event event) {
+		this.event = event;
+	}
+	
+	
+	public void changeLocation(String newlocation) {
+		event.setLocation(newlocation);
+	}
+	
+	public void changeTime(String newtime) {
+		event.setTime(newtime);
+	}
+	
+	public void changeName(String newname) {
+		event.setName(newname);
+	}
+	
+	public void changeDesc(String newdesc) {
+		event.setDescription(newdesc);
+	}
+	
+	public void changeIsPassed(Boolean passed) {
+		event.setIsPassedTime(passed);
+	}
+	
+	public void changeIsUpComing(Boolean upcoming) {
+		event.setIsUpComing(upcoming);
+	}
 }
