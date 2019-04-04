61c4e4726d97990cd50adceaf6018ad66bd6f59f
cbrandt6 <cbrandt6@ycp.edu>
2019-04-03 21:25:30 -0400

    Progress on schedule class

--- ycp_hacks/src/model/Schedule.java
+++ ycp_hacks/src/model/Schedule.java
@@ -1,5 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Schedule{
+	private ArrayList<Event> events;
+	
+	public Schedule() {
+		
+	}
+	
+	
+	//Get event what does this do?
+	public void addEvent(Event event) {
+		events.add(event);
+	}
+	
+	public void removeEvent(Event event){
+		events.remove(event);
+	}
+	
 	
 }
