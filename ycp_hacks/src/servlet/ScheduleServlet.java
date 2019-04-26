f0a574590f3924f94291934f2fb67e0d82a1ca60
cbrandt6 <cbrandt6@ycp.edu>
2019-04-26 17:32:04 -0400

    Committing to merge

--- ycp_hacks/src/servlet/ScheduleServlet.java
+++ ycp_hacks/src/servlet/ScheduleServlet.java
@@ -46,9 +46,14 @@ private static final long serialVersionUID = 1L;
 		
 		//Loads schedule from database into the model schedule
 		schedCont.loadSchedule(db);
-		//Tests to see if datetomillis actually works
-//		Event evnt = schedule.getEvent(1);
-//		System.out.println(evnt.dateToMillis());
+		//Displays event date in millis
+//		for (int i = 0; i < schedule.getSize(); i++) {
+//			Event evnt = schedule.getEvent(i);
+//			System.out.println(evnt.dateToMillis());
+//		}
+		
+		
+		
 		//Convert to collection to make it iterable in the jsp
 		List<Event> eventlist = schedule.getSchedule();
 		
